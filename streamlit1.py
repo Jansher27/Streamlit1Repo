52633e3edfed64ee48cd3c955a5b1a687c9fbf36
Jansher27 <141176378+Jansher27@users.noreply.github.com>
2024-04-01 18:31:19 -0700

    Add files via upload

--- streamlit1.py
+++ streamlit1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def main():
-    st.title("MotoGP Official News")
+    st.title("MotoGP Official News. After serioius delibration amongst Dorna, the new MotoGP Owners Liberty Media, its confirmed Marc Marquez was at fault caushing the two time back-to-back MotoGP champion, Pecco Bagnia, a pupil of the GOAT, to crash at the last round."")
     st.header("Rossi is the Greatest")
 
 if __name__ == "__main__":
