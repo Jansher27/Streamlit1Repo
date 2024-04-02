296da026c324cbc71079f7f12510e858a7d239e6
Jansher27 <141176378+Jansher27@users.noreply.github.com>
2024-04-01 18:50:21 -0700

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -4,7 +4,7 @@ def main():
     st.title("MotoGP Official News.") 
     st.text("""
     After serioius delibration amongst Dorna and Libery Media, the new owners of MotoGP,
-    it has been determined March Marquez was at fault for the crash with 2 time champion
+    it has been determined Marc Marquez was at fault for the crash with 2 time champion
     Pecco Bagnia. More to come soon.
     
     Additional informaiton will be coming soon through the media.""")
