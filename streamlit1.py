cac57ed826420f3775a5c429ba807d74ede4edee
Jansher27 <141176378+Jansher27@users.noreply.github.com>
2024-04-01 18:42:25 -0700

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 def main():
     st.title("MotoGP Official News.") 
-    st.text("After serioius delibration amongst Dorna and Libery Media, the new owners of MotoGP,
+    st.text("""After serioius delibration amongst Dorna and Libery Media, the new owners of MotoGP,
     it has been determined March Marquez 
-    was at fault for the crash with 2 time champion Pecco Bagnia. More to come soon.")
+    was at fault for the crash with 2 time champion Pecco Bagnia. More to come soon.""")
     st.header("Rossi is the Greatest")
 
 if __name__ == "__main__":
