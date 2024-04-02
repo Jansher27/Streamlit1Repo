73a52f47832f25aaca2e22111737a27371204ff0
Jansher27 <141176378+Jansher27@users.noreply.github.com>
2024-04-01 18:43:10 -0700

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -6,7 +6,7 @@ def main():
     After serioius delibration amongst Dorna and Libery Media, the new owners of MotoGP,
     it has been determined March Marquez 
     was at fault for the crash with 2 time champion Pecco Bagnia. More to come soon.""")
-    st.header("Rossi is the Greatest")
+    
 
 if __name__ == "__main__":
     main()
